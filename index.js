570094ade9eb260f1c6b32162ae27e007a4390a5
Jonathan W <jwilliams12117@gmail.com>
2022-02-24 20:10:21 -0700

    Finished user prompts for employees and employee details

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const employeeArray = [];
 //Function that generates the HTML page from answer object
 const generatePage = function() {
     //Run the code from src folder
-    
+
 };
 
 //Prompt user for employees...
@@ -43,22 +43,79 @@ const managerPrompt = () => {
     ])
 }
 
+//Prompt details of the employee
+const promptDetails = (employee) => {
+    console.log("Prompting for details about "+employee);
+    if (employee === "Engineer") {
+        inquirer.prompt([
+            {
+                type: "text",
+                name: "name",
+                message: "Enter the engineer's name"
+            },
+            {
+                type: "text",
+                name: "id",
+                message: "Enter the engineer's employee ID number."
+            },
+            {
+                type: "text",
+                name: "email",
+                message: "Enter the engineer's work email address."
+            },
+            {
+                type: "text",
+                name: "github",
+                message: "Enter the engineer's github username."
+            }
+        ]).then(promptEmployee);
+    }
+    else if (employee === "Intern") {
+        inquirer.prompt([
+            {
+                type: "text",
+                name: "name",
+                message: "Enter the intern's name"
+            },
+            {
+                type: "text",
+                name: "id",
+                message: "Enter the intern's employee ID number."
+            },
+            {
+                type: "text",
+                name: "email",
+                message: "Enter the intern's work email address."
+            },
+            {
+                type: "text",
+                name: "school",
+                message: "Enter the intern's school."
+            }
+        ]).then(promptEmployee);
+    }
+}
+
 //Prompt name, employee ID, email, and office number
 const promptEmployee = () => {
     inquirer.prompt(
         {
             type: "list",
-            name: "addEmployee",
+            name: "employeeType",
             message: "Choose to add an engineer or intern to the team, or finish building.",
             choices: ["Engineer", "Intern", "Finish"]
         }
     ).then(function(answer) {
-        console.log("Answer: "+answer.addEmployee);
-        console.log(answer.addEmployee);
+        console.log("Answer: "+answer.employeeType);
+        console.log(answer.employeeType);
 
-        if (answer.addEmployee != "Finish") {
-            employeeArray.push(answer.addEmployee);
-            promptEmployee();
+        if (answer.employeeType === "Engineer") {
+            promptDetails(answer.employeeType);
+            employeeArray.push(answer.employeeType);
+        }
+        else if (answer.employeeType === "Intern"){
+            promptDetails(answer.employeeType)
+            employeeArray.push(answer.employeeType);
         }
         else {
             console.log(employeeArray);
