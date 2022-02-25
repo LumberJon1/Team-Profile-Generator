bba2dde0d25b1e3f3aa80527b348c171240fcaf8
Jonathan W <jwilliams12117@gmail.com>
2022-02-24 19:33:21 -0700

    Restructured index.js to use inquirer prompts in order

--- index.js
+++ index.js
@@ -6,84 +6,70 @@ const Intern = require("./lib/Intern.js");
 const Prompts = require("./lib/Prompts.js");
 
 
+
+//Array that holds employees
+const employeeArray = [];
+
 //Function that generates the HTML page from answer object
 const generatePage = function() {
     //Run the code from src folder
+    
 };
 
-const prompt = new Prompts;
-prompt.promptEmployee()
+//Prompt user for employees...
+//Section for the manager
+const managerPrompt = () => {
+    return inquirer.prompt([
+            {
+            type: "text",
+            name: "name",
+            message: "Enter the manager's name"
+        },
+        {
+            type: "text",
+            name: "id",
+            message: "Enter the manager's employee ID number."
+        },
+        {
+            type: "text",
+            name: "email",
+            message: "Enter the manager's work email address."
+        },
+        {
+            type: "text",
+            name: "officeNumber",
+            message: "Enter the manager's office number."
+        }
+    ])
+}
 
-// //Array that holds employees
-// employeeArray = [];
+//Prompt name, employee ID, email, and office number
+const promptEmployee = () => {
+    inquirer.prompt(
+        {
+            type: "list",
+            name: "addEmployee",
+            message: "Choose to add an engineer or intern to the team, or finish building.",
+            choices: ["Engineer", "Intern", "Finish"]
+        }
+    ).then(function(answer) {
+        console.log("Answer: "+answer.addEmployee);
+        console.log(answer.addEmployee);
 
-// //Prompt user for employees...
-// //Section for the manager
-// const managerPrompt = () => {
-//     return inquirer.prompt([
-//             {
-//             type: "text",
-//             name: "name",
-//             message: "Enter the manager's name"
-//         },
-//         {
-//             type: "text",
-//             name: "id",
-//             message: "Enter the manager's employee ID number."
-//         },
-//         {
-//             type: "text",
-//             name: "email",
-//             message: "Enter the manager's work email address."
-//         },
-//         {
-//             type: "text",
-//             name: "officeNumber",
-//             message: "Enter the manager's office number."
-//         }
-//     ])
-// }
+        if (answer.addEmployee != "Finish") {
+            employeeArray.push(answer.addEmployee);
+            promptEmployee();
+        }
+        else {
+            console.log(employeeArray);
+        }
+    })
+}
 
-// //Prompt name, employee ID, email, and office number
-// const promptEmployee = () => {
-//     return inquirer.prompt([
-//         {
-//             type: "list",
-//             name: "addEmployee",
-//             message: "Choose to add an engineer or intern to the team, or finish building.",
-//             choices: ["Engineer", "Intern", "Finish"]
-//         }
-//     ])
-// }
+//When finished, generate the HTML
 
-// const addEmployee = (employee) => {
-    
-//     if (employee.addEmployee === "Engineer") {
-//         //Add an engineer
-//         console.log("Adding an engineer to the team.");
-//         promptEmployee();
-//     }
-//     else if (employee.addEmployee === "Intern") {
-//         //Add intern
-//         console.log("Adding an intern to the team.");
-//         promptEmployee();
-//     }
-//     else {
-//         return;
-//     }
-// }
-// //IF Engineer, prompt for name, ID, email, and github
-
-// //Else if Intern, prompt for name, ID, email, and school
-
-// //When finished, generate the HTML
-
-// managerPrompt()
-// .then(answer => {
-//     employeeArray.push(answer);
-//     promptEmployee().then(answer => {
-//         if (answer.addEmployee != "Finish") {
-//             console.log("Not finished.")
-//         }    
-//     });
-// });
+managerPrompt().then(function(answer) {
+    //do something with answer
+    console.log(answer);
+    promptEmployee();
+});
